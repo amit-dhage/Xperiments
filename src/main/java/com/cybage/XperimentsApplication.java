941351ad4a7787a9abb1865b663a4b1614002f54
Tukaram Dhage <trdhage@outlook.com>
2017-03-09 10:05:57 +0530

    Intial awesome commit

---
+++ src/main/java/com/cybage/XperimentsApplication.java
@@ -0,0 +1,12 @@
+package com.cybage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XperimentsApplication.class, args);
+	}
+}
