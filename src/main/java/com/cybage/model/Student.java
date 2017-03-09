3cc8b2937caee01d1f77f001d5d4437f89c56ef5
Tukaram Dhage <trdhage@outlook.com>
2017-03-09 11:27:47 +0530

    Added Model class

---
+++ src/main/java/com/cybage/model/Student.java
@@ -0,0 +1,65 @@
+package com.cybage.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by tukaramdh on 3/9/2017.
+ */
+public class Student implements Serializable {
+    private int id;
+    private String name;
+    private String address;
+
+    public Student() {
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+}
